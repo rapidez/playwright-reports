92beb30e73e6807ac1c22329da3000ca0a6630d5
Roy Duineveld <royduineveld@gmail.com>
2025-10-02 14:23:23 +0200

    Keep only 14 days of reports

--- show-old-folders.sh
+++ show-old-folders.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Navigate to your repository
-# Find folders in "rapidez" that are older than a month
+# Find folders in "rapidez" that are older than a 14 days
 git log --name-only --pretty=format: --diff-filter=A rapidez | awk '{print $1}' | sort | uniq | while read -r file; do
     folder=$(dirname "$file")
     last_commit_date=$(git log -1 --format="%ai" -- "$file")
     last_commit_epoch=$(date -d "$last_commit_date" +%s)
-    one_month_ago=$(date -d "30 days ago" +%s)
-    if [ "$last_commit_epoch" -lt "$one_month_ago" ]; then
+    old=$(date -d "14 days ago" +%s)
+    if [ "$last_commit_epoch" -lt "$old" ]; then
         echo "$folder"
     fi
 done
